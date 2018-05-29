1e6ee46cbcdcfb7e4879da1b6d730a7008326122
chibiegg <chibiegg@chibiegg.net>
2018-05-29 22:45:53 +0900

    取りこぼさないようにポーリング方式に変更

--- lua/notify.lua
+++ lua/notify.lua
@@ -5,7 +5,7 @@ function find_latest_path(basedir)
     local max_mod = 0
     local max_mod_path = nil
     for file in lfs.dir(basedir) do
-        local path = PHOTO_TARGET_PATH.."/"..file
+        local path = basedir.."/"..file
         local last_mod = lfs.attributes(path, "modification")
         if last_mod > max_mod then
             max_mod = last_mod
@@ -27,5 +27,24 @@ function post_text(url, payload)
   }
 end
 
+function get_file_number(filename)
+  return tonumber(string.sub(string.lower(filename), -8, -5), 10)
+end
+
 filename = find_latest_path(PHOTO_TARGET_PATH)
-post_text(NOTIFY_URL, filename)
+local current_number = get_file_number(filename) - 1
+
+while true
+
+  for file in lfs.dir(PHOTO_TARGET_PATH) do
+      local path = PHOTO_TARGET_PATH.."/"..file
+      local suffix = string.sub(string.lower(path), -3)
+      local number = get_file_number(path)
+      if suffix == "jpg" and number > current_number then
+        post_text(NOTIFY_URL, path)
+        current_number = number
+      end
+  end
+
+  sleep(1000)
+do
